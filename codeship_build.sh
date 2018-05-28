3b00c0f115fbc2ae353f5d68af15120429a22695
Bart Kummel <bart.kummel@simacan.com>
2018-05-28 09:20:38 +0200

    Added version tag to docker push

--- codeship_build.sh
+++ codeship_build.sh
@@ -2,6 +2,11 @@
 
 eval $(aws ecr get-login --no-include-email --region us-east-1)
 
-docker build -t otm-spec-server . && \
-docker tag otm-spec-server 675074472457.dkr.ecr.us-east-1.amazonaws.com/otm-spec-server && \
-docker push 675074472457.dkr.ecr.us-east-1.amazonaws.com/otm-spec-server
+versionString=$(eval git describe)
+version=${versionString##*/}
+
+docker build -t otm-spec-server .
+docker tag otm-spec-server 675074472457.dkr.ecr.us-east-1.amazonaws.com/otm-spec-server:latest
+docker tag otm-spec-server 675074472457.dkr.ecr.us-east-1.amazonaws.com/otm-spec-server:$version
+docker push 675074472457.dkr.ecr.us-east-1.amazonaws.com/otm-spec-server:latest
+docker push 675074472457.dkr.ecr.us-east-1.amazonaws.com/otm-spec-server:$version
